c13c66c0e6e241df0d22e1921f0f39c13b37e0bb
DanielEloy <58008758+DanielEloy@users.noreply.github.com>
2020-10-11 15:59:31 -0300

    Create agenda.sql

---
+++ agenda.sql
@@ -0,0 +1,9 @@
+create database agenda;
+use agenda;
+create table contato(
+id int not null auto_increment,
+nome varchar(50)null,
+telefone varchar(20)null,
+primary key(id)
+)
+select * from contato; 
